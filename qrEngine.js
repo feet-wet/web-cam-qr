eeba50c52a21d2d9d8b7ee60bb155ad7e5df9a1b
sergey.illarionov <easthunch@gmail.com>
2015-08-12 13:49:36 +0300

    fix: refactoring of BitMatrix

--- qrEngine.js
+++ qrEngine.js
@@ -792,7 +792,7 @@ var QRWebScannerEngine = (function () {
         this.ordinal=function() {
             return this.ordinal_Renamed_Field;
         }
-    }
+    };
 
     ErrorCorrectionLevel.forBits=function( bits) {
         if (bits < 0 || bits >= FOR_BITS.length) {
@@ -801,17 +801,10 @@ var QRWebScannerEngine = (function () {
         return FOR_BITS[bits];
     };
 
-    var L = new ErrorCorrectionLevel(0, 0x01, "L");
-    var M = new ErrorCorrectionLevel(1, 0x00, "M");
-    var Q = new ErrorCorrectionLevel(2, 0x03, "Q");
-    var H = new ErrorCorrectionLevel(3, 0x02, "H");
-    var FOR_BITS = [M, L, H, Q];
-
-
     //
 
 
-    function BitMatrix( width,  height) {
+    var BitMatrix = function ( width,  height) {
         if(!height) height=width;
 
         if (width < 1 || height < 1) {
@@ -820,9 +813,8 @@ var QRWebScannerEngine = (function () {
         this.width = width;
         this.height = height;
         var rowSize = width >> 5;
-        if ((width & 0x1f) != 0) {
-            rowSize++;
-        }
+        if ((width & 0x1f) != 0) rowSize++;
+
         this.rowSize = rowSize;
         this.bits = new Array(rowSize * height);
         for(var i=0;i<this.bits.length;i++)
@@ -855,36 +847,26 @@ var QRWebScannerEngine = (function () {
         };
         this.clear=function() {
             var max = this.bits.length;
-            for (var i = 0; i < max; i++)
-            {
+            for (var i = 0; i < max; i++) {
                 this.bits[i] = 0;
             }
         };
         this.setRegion=function( left,  top,  width,  height) {
-            if (top < 0 || left < 0)
-            {
-                throw "Left and top must be nonnegative";
-            }
-            if (height < 1 || width < 1)
-            {
-                throw "Height and width must be at least 1";
-            }
-            var right = left + width;
-            var bottom = top + height;
-            if (bottom > this.height || right > this.width)
-            {
-                throw "The region must fit inside the matrix";
-            }
-            for (var y = top; y < bottom; y++)
-            {
+            if (top < 0 || left < 0) throw "Left and top must be nonnegative";
+            if (height < 1 || width < 1) throw "Height and width must be at least 1";
+
+            var right = left + width,
+                bottom = top + height;
+            if (bottom > this.height || right > this.width) throw "The region must fit inside the matrix";
+
+            for (var y = top; y < bottom; y++) {
                 var offset = y * this.rowSize;
-                for (var x = left; x < right; x++)
-                {
+                for (var x = left; x < right; x++) {
                     this.bits[offset + (x >> 5)] |= 1 << (x & 0x1f);
                 }
             }
         }
-    }
+    };
 
     //
 
